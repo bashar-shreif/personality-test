3c9f05f97fd6b792c437cf6ac6923e3181b2f180
Bashar Shreif <basharchreif70@gmail.com>
2025-11-25 02:08:58 +0200

    Adding app navigation and logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:personality_test/personality_test_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PersonalityTestApp());
 }
