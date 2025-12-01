4cc0516ae7e319bbf4a2a4d4dc771bf7e2b6bf83
Bashar Shreif <basharchreif70@gmail.com>
2025-12-02 00:53:55 +0200

    Finalizing app functionality

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -1,3 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:personality_test/models/personality.dart';
+import 'package:personality_test/models/question.dart';
+import 'package:personality_test/data/questions.dart';
+import 'package:personality_test/widgets/custom_button.dart';
 
-class QuestionScreen extends StatelessWidget {}
+class QuestionScreen extends StatelessWidget {
+  int currentPosition;
+  final Function(Personality) updateScore;
+  List<Question> data = questions;
+
+  QuestionScreen({required this.currentPosition, required this.updateScore});
+
+  @override
+  Widget build(BuildContext context) {
+    Question question = data[currentPosition];
+    return Column(
+      children: [
+        Text(question.question),
+        CustomButton(
+          btn_txt: question.answers[0].text,
+          on_pressed: updateScore(question.answers[0].personality),
+        ),
+        CustomButton(
+          btn_txt: question.answers[1].text,
+          on_pressed: updateScore(question.answers[1].personality),
+        ),
+        CustomButton(
+          btn_txt: question.answers[2].text,
+          on_pressed: updateScore(question.answers[2].personality),
+        ),
+        CustomButton(
+          btn_txt: question.answers[3].text,
+          on_pressed: updateScore(question.answers[3].personality),
+        ),
+      ],
+    );
+  }
+}
